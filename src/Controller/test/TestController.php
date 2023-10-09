df57e8f3178690cfd5d88e1e5292d6764748ca35
Sasha Melnicova <sa1524517@gmail.com>
2023-10-09 18:20:37 +0300

    add test controller

--- src/Controller/test/TestController.php
+++ src/Controller/test/TestController.php
@@ -4,7 +4,14 @@ namespace App\Controller\test;
 
 use App\Controller\UserController;
 use App\Helpers\CheckTokenExpiration;
+use App\Helpers\GeneratorFiles;
+use App\Helpers\Speechkit;
 use App\Helpers\UploadFile;
+use App\Models\ContentVideo;
+use App\Models\ListImage;
+use App\Models\ListMusic;
+use App\Models\ListVideo;
+use App\Models\TextVideo;
 use App\Models\User;
 use Exception;
 use GuzzleHttp\Client;
@@ -19,37 +26,72 @@ class TestController extends UserController
 
     public function action(): ResponseInterface
     {
-        $row['text'] = 'Мем — единица значимой для культуры информации.з предложил идею о том';
-        $this->client = new Client();
+        $videoId = 7;
+        $video = ContentVideo::findAllDataByID($videoId);
+        $images = ListImage::findAllByContentId($video['content_id']);
 
-        $response = $this->client->post('https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize',
-            [
-                'headers' => [
-                    'Authorization' => 'Bearer t1.9euelZqLjs-OjIvMk46JmpCPmseei-3rnpWamJiTmpbNnImPnZPPjMbHnJDl8_dhZgRX-e9uZSJf_d3z9yEVAlf5725lIl_9zef1656VmpCQmomZzcrMypuZmZmKm5SU7_zF656VmpCQmomZzcrMypuZmZmKm5SU.jyNwOhBbREoIrIBwyS8xDo6cnKK40GDLm11tv9bieKXsMYjcllOV_8CC7VxQu4aYIT8VskxuxsPy959G41r5Dw',
-                    'x-folder-id' => 'b1glckrv5eg7s4kkhtpn',
-                    'Content-Type' => 'application/x-www-form-urlencoded'
-                ],
-                'form_params' => [
-                    'text' => $row['text'],
-                    'format' => 'mp3',
-                    'lang' => 'ru-RU',
-                    'voice' => 'jane',
-                    'emotion' => 'good',
-                    'folderId' => 'b1glckrv5eg7s4kkhtpn'
-                ]
-            ]);
+        $logo = [];
+        $slides = [];
 
-        $length = file_put_contents(DIRECTORY_MUSIC . 'music.mp3', $response->getBody()->getContents());
-//        $body = $response->getBody();
-//        var_dump($body);
-//        $data = $body->getContents();
-//        var_dump($data);
-//        $filename = UploadFile::action(DIRECTORY_IMG, $data , 8);
-        var_dump($filename);
-        exit();
+        foreach ($images as $image) {
+            if ($image['type'] == 'logo') {
+                $logo[] = $image['file_name'];
+            }
 
-//        return $this->respondWithError(215);
+            if ($image['type'] == 'slide') {
+                $slides[] = $image['file_name'];
+            }
+        }
+
+        $sound = ListMusic::findAllByContentId($video['content_id']);
+        $video['video'] = ListVideo::findAllByContentId($video['content_id']);
+
+        $videoBackground = [];
+        $videoStart = [];
+        $videoEnd = [];
+
+        foreach ($video['video'] as $additionalVideo) {
+            if ($additionalVideo['type'] == 'content') {
+                $videoBackground[] = $additionalVideo['file_name'];
+            }
+
+            if ($additionalVideo['type'] == 'start') {
+                $videoStart[] = $additionalVideo['file_name'];
+            }
+
+            if ($additionalVideo['type'] == 'end') {
+                $videoEnd[] = $additionalVideo['file_name'];
+            }
+        }
+
+        $generatorFiles = new GeneratorFiles($video['content_id']);
+
+        $timeVoice = '54.648010530935';
+        if ($video['status_text'] == 'false' || $video['status_text'] == 'создано') {
+
+            TextVideo::changeTextStatus($video['text_id'], 'в обработке');
+            $textData = $generatorFiles->generatorTextForTitre($video['text'], $video['text_id']);
+
+            if ($textData['status']) {
+                TextVideo::changeTextStatus($video['text_id'], 'обработано');
+            } else {
+                TextVideo::changeTextStatus($video['text_id'], 'ошибка');
+                $this->respondWithError(400, 'Ошибка генерации субтитров');
+            }
+        }
+
+        if ($video['type_background'] == 'slide_show') {
+            $slideshow = $generatorFiles->generatorSladeShow($slides, $sound[0]['file_name'], $timeVoice);
+
+            var_dump($slideshow);
+            if (!$slideshow['status']) {
+                return $this->respondWithError(400, 'Ошибка генерации слайдшоу');
+            }
+
+            $resultName = $slideshow['fileName'];
+        }
+
+        return $this->respondWithData($resultName);
     }
-
 }
 
