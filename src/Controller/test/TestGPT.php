f889d15863d31339eeee351932a7b76a65ff7e8f
Sasha Melnicova <sa1524517@gmail.com>
2023-10-31 21:24:34 +0300

    fix test for gpt

--- src/Controller/test/TestGPT.php
+++ src/Controller/test/TestGPT.php
@@ -22,26 +22,26 @@ class TestGPT extends UserController
 
         $client = new Client();
 
-        $response = $client->post('http://45.92.176.207:4749/api/main',
-            [
-                'headers' => [
-                    'Content-Type' => 'application/json'
-                ],
-                'json' => [
-                    'title' => $query
-                ]
-            ]);
+//        $response = $client->post('http://45.92.176.207:4749/api/main',
+//            [
+//                'headers' => [
+//                    'Content-Type' => 'application/json'
+//                ],
+//                'json' => [
+//                    'title' => $query
+//                ]
+//            ]);
+//
+//        $responseData = json_decode($response->getBody()->getContents(), true);
+//
+//        print_r($responseData);
+//        file_put_contents(DIRECTORY_RESULT_CONTENT . $query . '.txt',$responseData);
+//
+//        return $this->respondWithData('Ok');
 
-        $responseData = json_decode($response->getBody()->getContents(), true);
-
-        print_r($responseData);
-        file_put_contents(DIRECTORY_RESULT_CONTENT . $query . '.txt',$responseData);
-
-        return $this->respondWithData('Ok');
-
-//        $response = $client->get('http://127.0.0.1:5000/api/main');
+        $response = $client->get('http://127.0.0.1:5000/api/main');
 
 
-//        return $this->respondWithData($response);
+        return $this->respondWithData($response);
     }
 }
