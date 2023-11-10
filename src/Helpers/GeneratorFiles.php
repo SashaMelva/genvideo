1d96a06b268ea3bc5b7a1194d901227a140020aa
Sasha Melnicova <sa1524517@gmail.com>
2023-11-10 12:29:03 +0300

    fix generation

--- src/Helpers/GeneratorFiles.php
+++ src/Helpers/GeneratorFiles.php
@@ -218,7 +218,7 @@ class GeneratorFiles
             return ['status' => false, 'command' => $ffmpeg];
         }
 
-        unlink(DIRECTORY_VIDEO . $videoName);
+        unlink(DIRECTORY_VIDEO . $videoName. '.mp4');
         return ['fileName' => $resultName, 'status' => true];
     }
 
