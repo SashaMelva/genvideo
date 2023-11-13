310d6df4ded989d7ab763f6871e08897d255fb61
Sasha Melnicova <sa1524517@gmail.com>
2023-11-13 15:10:29 +0300

    fix generation

--- src/Controller/Files/LoadVideo.php
+++ src/Controller/Files/LoadVideo.php
@@ -36,7 +36,7 @@ class LoadVideo extends UserController
                     header('Access-Control-Expose-Headers: Content-Disposition');
                     header('Content-Description: File Transfer');
                     header('Content-Type: application/octet-stream');
-                    header('Content-Disposition: attachment; filename=' . is_null($video['name']) ? 'Новое видео' : $video['name'] . '.mp4');
+                    header('Content-Disposition: attachment; filename=' . $video['name'] . '.mp4');
                     header('Content-Transfer-Encoding: binary');
                     header('Expires: 0');
                     header('Cache-Control: must-revalidate');
