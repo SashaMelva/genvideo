2260d865e7653db0db558570f6fa30f55fc39833
Sasha Melnicova <sa1524517@gmail.com>
2023-11-13 16:14:27 +0300

    fix generation

--- src/Controller/Video/DownloadVideo.php
+++ src/Controller/Video/DownloadVideo.php
@@ -42,7 +42,7 @@ class DownloadVideo extends UserController
                     header('Access-Control-Expose-Headers: Content-Disposition');
                     header('Content-Description: File Transfer');
                     header('Content-Type: application/octet-stream');
-                    header('Content-Disposition: attachment; filename=' . $video['name'] . '.mp4');
+                    header('Content-Disposition: attachment; filename=' . $video['content_name'] . '.mp4');
                     header('Content-Transfer-Encoding: binary');
                     header('Expires: 0');
                     header('Cache-Control: must-revalidate');
