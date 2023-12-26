f834d296d773edf6cd0854671c8a48a0a0046685
Соколов Сергей <serzblog@yandex.ru>
2023-12-26 11:19:42 +0300

    fix

--- src/Helpers/GeneratorFiles.php
+++ src/Helpers/GeneratorFiles.php
@@ -346,7 +346,7 @@ class GeneratorFiles
             $fontSize = 84;
         } elseif ($whidthPreview >= 1501 && $whidthPreview <= 2500) {
             $marginTop = 80;
-            $placeTop = 120;
+            $placeTop = 140;
             $marginLeft = 40;
             $fontSize = 126;
         } else {
